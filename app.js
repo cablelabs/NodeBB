f9ec1ad18775158cae2d44e150e6ff03202d6845
Santhosh Kulasekar <santhosh.kulasekar@gmail.com>
2014-10-02 17:02:58 -0400

    Removed node_modules for cleanup.

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ function loadConfig() {
 
 	nconf.defaults({
 		base_dir: __dirname,
-		themes_path: path.join(__dirname, 'node_modules'),
+		themes_path: path.join(__dirname, 'public'),
 		upload_url: '/uploads/',
 		views_dir: path.join(__dirname, 'public/templates'),
         static_dir: path.join(__dirname, 'static')
