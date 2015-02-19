9e77636d1f87e4dd7b2399614e85191907f4b7a8
Santhosh Kulasekar <santhosh.kulasekar@gmail.com>
2015-02-19 12:01:42 -0500

    Some clean up.

--- public/designer/scripts/filters/string-filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('stringFilters', [])
-  .filter('dasherize', function() {
-    return function (input) {
-      return input ? input.toLowerCase().trim().replace(/\s/g, '-') : '';
-    };
-  });
